24f9171397f9eff583b28e2f493300e78c5cf3b5
samurai29z <djamilatou2912@gmail.com>
2023-01-02 23:07:54 +0100

    first commit

--- server/server.js
+++ server/server.js
@@ -8,6 +8,35 @@ app.use(express.json());
 app.use(require("./routes/record"));
 // get driver connection
 const dbo = require("./db/conn");
+
+//inserer
+dotenv.config();
+app.use(express.json());
+app.use("/images", express.static(path.join(__dirname, "/images")));
+
+// get driver connection
+//const dbo = require("./db/conn");
+
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, "images");
+  },
+  filename: (req, file, cb) => {
+    cb(null, Date.now() + '-' + file.originalname);
+  },
+});
+
+const upload = multer({ storage}).single('file');
+
+app.post("/api/upload", (req, res) => {
+  upload(req, res, (err) =>{
+    if (err) {
+      return res.status(500).json(err)
+    }
+    return res.status(200).send(req.file)
+  })
+});
+//fin insertion
  
 app.listen(port, () => {
   // perform a database connection when server starts
